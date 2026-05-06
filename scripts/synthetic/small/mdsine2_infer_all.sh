de7b32df762d9c9bf93a2268985b0b13cd009819
joaovaleriano <joaopedrovm98@gmail.com>
2026-05-06 18:27:10 +0200

    .

--- scripts/synthetic/small/mdsine2_infer_all.sh
+++ scripts/synthetic/small/mdsine2_infer_all.sh
@@ -17,24 +17,32 @@ for (( trial = 0; trial < ${NUM_SAMPLE_TRIALS}; trial++ )); do
 
 		# ======= Fit NegBin qPCR model
 		echo "[*] Fitting Negative binomial model."
-		mdsine2 infer-negbin --input ${replicates} --seed ${negbin_seed} --burnin 10 --n-samples 20 --checkpoint 1 --basepath $negbin_out_dir
+		mdsine2 infer-negbin --input ${replicates} --seed ${negbin_seed} --burnin 2000 --n-samples 5000 --checkpoint 1 --basepath $negbin_out_dir --multiprocessing --log-every 500
 		mdsine2 visualize-negbin --chain "${negbin_out_dir}/replicate-${noise_level}/mcmc.pkl" --output-basepath "${negbin_out_dir}/replicate-${noise_level}"
 
 		# ======= Run inference
 		echo "[*] Running non-clustered mdsine2 inference (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-		python synthetic/helpers/inference.py \
+		# python synthetic/helpers/inference.py \
+
+		mdsine2 infer \
 				--input $dataset \
 				--negbin ${negbin_out_dir}/replicate-${noise_level}/mcmc.pkl \
 				--seed 0 \
-				--burnin 10 \
-				--n-samples 20 \
+				--burnin 5000 \
+				--n-samples 15000 \
 				--checkpoint 1 \
 				--multiprocessing 0 \
 				--basepath $inference_out_dir \
 				--interaction-ind-prior "weak-agnostic" \
 				--perturbation-ind-prior "weak-agnostic" \
-				# --nomodules \
+				--nomodules \
+				--multiprocessing 1 \
+				--log-every 500
 				# --time_mask ${DATASET_DIR}/time_mask.tsv
+		#
+		
+		# mdsine2 infer --input $dataset
+
 		echo "[*] Finished mdsine2 inference."
 	done
 done
